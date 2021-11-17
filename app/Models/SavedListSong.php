9e97f17e6116beed81fbe422bb81d9aaf16c535e
Diony Busker <dionybusker@gmail.com>
2021-11-17 17:47:10 +0100

    Added eloquent relationships: SavedListSong-SavedList, SavedListSong-Song

--- app/Models/SavedListSong.php
+++ app/Models/SavedListSong.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class SavedListSong extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function savedList()
+    {
+        return $this->belongsTo(SavedList::class);
+    }
+
+    public function song()
+    {
+        return $this->hasMany(Song::class);
+    }
 }
