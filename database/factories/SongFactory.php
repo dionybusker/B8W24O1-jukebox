3541396560f8ee0242de426b5a48ce3c51530bd5
Diony Busker <dionybusker@gmail.com>
2021-11-17 23:29:40 +0100

    Created a Song factory to create fake data for database Song table

---
+++ database/factories/SongFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Genre;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SongFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'genre_id' => Genre::factory(),
+            'name' => $this->faker->sentence(),
+            'artist' => $this->faker->name(),
+            'duration' => $this->faker->numberBetween(1,10)
+        ];
+    }
+}
