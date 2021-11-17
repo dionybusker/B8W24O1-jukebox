15c54ab736ee960b19886e982a233ee3c9905a51
Diony Busker <dionybusker@gmail.com>
2021-11-17 23:33:52 +0100

    Added Tailwind CSS

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,6 +1,13 @@
 <!doctype html>
 
-<title>Jukebox</title>
+<head>
+    <title>Jukebox</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+
 
 <body>
     {{ $slot }}
