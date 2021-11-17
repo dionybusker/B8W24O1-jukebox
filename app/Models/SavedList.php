e13a3bc0a5b57c323eb24063d22a89d8d39aec5a
Diony Busker <dionybusker@gmail.com>
2021-11-17 17:46:37 +0100

    Added eloquent relationship: SavedList-User

--- app/Models/SavedList.php
+++ app/Models/SavedList.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class SavedList extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
