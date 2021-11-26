a2b3f81593012b9d4c6e168d6f4827b4c33ca878
Diony <dionybusker@gmail.com>
2021-11-26 10:16:30 +0100

    View all Genres in a table (genres page)

--- resources/views/genres.blade.php
+++ resources/views/genres.blade.php
@@ -1,13 +1,27 @@
 <x-layout>
-    <h1>Jukebox</h1>
+    <section class="container w-full bg-gray-100 shadow-md">
+        <div class="px-10 py-8">
+            <div class="flex justify-center mx-auto text-xl font-semibold mb-5">Genres</div>
 
-    <ul>
-        @foreach ($genres as $genre)
-            <li>
-                {{ $genre->name }}
-            </li>
-        @endforeach
-    </ul>
-
-    <a href="/songs">Go to Songs</a>
+            <div class="flex justify-center mx-auto">
+                <div class="flex flex-col">
+                    <div class="w-full">
+                        <div class="border-b border-gray-200 shadow">
+                            <x-table.table :headers="['ID', 'Name', 'Find songs']">
+                                @foreach ($genres as $genre)
+                                    <tr class="whitespace-nowrap">
+                                        <x-table.td>{{ $genre->id }}</x-table.td>
+                                        <x-table.td>{{ $genre->name }}</x-table.td>
+                                        <x-table.td>
+                                            <x-table.td-search></x-table.td-search>
+                                        </x-table.td>
+                                    </tr>
+                                @endforeach
+                            </x-table.table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </x-layout>
