e2c5d7df753d6d18bcda1af97dc9dd7370d45ea6
Diony <dionybusker@gmail.com>
2021-12-08 11:33:56 +0100

    Made cards for songs instead of a table

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -1,32 +1,65 @@
-<x-layout>
-    <section class="container w-full bg-gray-100 shadow-md">
-        <div class="px-10 py-8">
-            <div class="flex justify-center mx-auto text-xl font-semibold mb-5">Songs</div>
+<x-app-layout>
+    <x-slot name="header">
+{{--        @include('_header')--}}
 
-            <div class="flex justify-center mx-auto">
-                <div class="flex flex-col">
-                    <div class="w-full">
-                        <div class="border-b border-gray-200 shadow">
-                            <x-table.table :headers="['Name', 'Artist', 'Genre', 'Duration', 'Details', 'Add to playlist']">
-                                @foreach ($songs as $song)
-                                    <tr class="whitespace-nowrap">
-                                        <x-table.td>{{ $song->name }}</x-table.td>
-                                        <x-table.td>{{ $song->artist }}</x-table.td>
-                                        <x-table.td>{{ $song->genre->id }}</x-table.td>
-                                        <x-table.td>{{ $song->duration }}</x-table.td>
-                                        <x-table.td>
-                                            <x-table.td-info></x-table.td-info>
-                                        </x-table.td>
-                                        <x-table.td>
-                                            <x-table.td-add></x-table.td-add>
-                                        </x-table.td>
-                                    </tr>
-                                @endforeach
-                            </x-table.table>
-                        </div>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Songs') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="lg:w-2/3 w-full mx-auto flex items-center lg:grid lg:grid-cols-4">
+                        @foreach ($songs as $song)
+                            <div class="p-2">
+                                <div class="bg-white rounded-lg overflow-hidden shadow-lg border border-gray-200">
+                                    <div class="space-x-2 px-4 pt-2">
+                                        <a href="#genre" class="px-3 py-0.5 bg-white border border-blue-500 rounded-full text-blue-400 hover:bg-blue-100 hover:text-blue-500 text-xs uppercase font-semibold">
+                                            {{ $song->genre->name }}
+                                        </a>
+                                    </div>
+
+                                    <div class="px-4 py-2 overflow-auto h-40">
+                                        <p class="font-semibold">{{ $song->name }}</p>
+                                        <p>{{ $song->artist }}</p>
+                                        <p>{{ $song->duration }} minutes</p>
+                                    </div>
+
+                                    <form method="post" action="{{ route('songs') }}/{{ $song->id }}">
+                                        @csrf
+                                        <div class="w-full flex text-sm uppercase font-semibold">
+    {{--                                         href="/songs/add/{{ $song->id }}" --}}
+                                            <button class="w-full bg-blue-600 hover:bg-blue-500 text-white text-center py-1">
+                                                <i class="fas fa-plus"></i>
+                                            </button>
+    {{--                                        <a href="#delete" class="w-1/2 bg-red-600 hover:bg-red-500 text-white text-center py-1">--}}
+    {{--                                            <i class="fas fa-trash-alt"></i>--}}
+    {{--                                        </a>--}}
+                                        </div>
+                                    </form>
+
+    {{--                                <div class="w-full flex text-sm uppercase font-semibold">--}}
+    {{--                                    <form action="/songs/add/{{ $song->id }}" method="post" class="w-1/2 bg-blue-600 hover:bg-blue-500 text-white text-center py-1">--}}
+    {{--                                        <button>--}}
+    {{--                                            <i class="fas fa-plus"></i>--}}
+    {{--                                        </button>--}}
+    {{--                                    </form>--}}
+
+    {{--                                    <form action="#" method="post" class="w-1/2 bg-red-600 hover:bg-red-500 text-white text-center py-1">--}}
+    {{--                                        <button>--}}
+    {{--                                            <i class="fas fa-trash-alt"></i>--}}
+    {{--                                        </button>--}}
+    {{--                                    </form>--}}
+    {{--                                </div>--}}
+
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-</x-layout>
+    </div>
+</x-app-layout>
