a3a4dbc5119b92eec3a3501e49e89e3d67558a77
Diony <dionybusker@gmail.com>
2021-12-08 11:36:28 +0100

    Added href

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
         <script src="https://kit.fontawesome.com/8eccf2802e.js" crossorigin="anonymous"></script>
+        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <title>Silencify</title>
     </head>
     <body>
@@ -35,16 +36,16 @@
                         <a href="/songs" class="block lg:inline-block lg:text-center lg:w-24 lg:mt-0 lg:py-2 py-3 pl-2 lg:px-3 rounded hover:bg-gray-200 hover:text-gray-800">
                             Songs
                         </a>
-                        <a href="#" class="block lg:inline-block lg:text-center lg:w-24 lg:mt-0 lg:py-2 py-3 pl-2 lg:px-3 rounded hover:bg-gray-200 hover:text-gray-800">
+                        <a href="/playlists" class="block lg:inline-block lg:text-center lg:w-24 lg:mt-0 lg:py-2 py-3 pl-2 lg:px-3 rounded hover:bg-gray-200 hover:text-gray-800">
                             Playlists
                         </a>
                     </div>
 
                     <div class="text-sm lg:flex-shrink">
-                        <a href="#" class="block lg:inline-block lg:text-center lg:w-20 lg:mt-0 lg:px-3 lg:py-1 pl-2 py-3 rounded lg:uppercase lg:text-xs lg:font-semibold lg:hover:bg-blue-600 lg:hover:text-white hover:bg-gray-200 hover:text-gray-800">
+                        <a href="/login" class="block lg:inline-block lg:text-center lg:w-20 lg:mt-0 lg:px-3 lg:py-1 pl-2 py-3 rounded lg:uppercase lg:text-xs lg:font-semibold lg:hover:bg-blue-600 lg:hover:text-white hover:bg-gray-200 hover:text-gray-800">
                             Login
                         </a>
-                        <a href="#" class="block lg:inline-block lg:text-center lg:w-20 lg:text-white lg:mt-0 lg:ml-3 lg:px-3 lg:py-1 pl-2 py-3 rounded lg:uppercase lg:text-xs lg:font-semibold lg:bg-blue-600 lg:hover:bg-blue-500 lg:hover:text-white hover:bg-gray-200 hover:text-gray-800">
+                        <a href="/register" class="block lg:inline-block lg:text-center lg:w-20 lg:text-white lg:mt-0 lg:ml-3 lg:px-3 lg:py-1 pl-2 py-3 rounded lg:uppercase lg:text-xs lg:font-semibold lg:bg-blue-600 lg:hover:bg-blue-500 lg:hover:text-white hover:bg-gray-200 hover:text-gray-800">
                             Register
                         </a>
                     </div>
@@ -54,9 +55,9 @@
 
         {{ $slot }}
 
-        <footer class="px-6 py-2">
-            <div class="bg-white text-center mt-5 mb-1 text-xs">
-                &copy; Silencify, Diony Busker 2021
+        <footer class="bg-gray-50 px-6 py-5">
+            <div class="text-center text-sm">
+                &copy; 2021 - Diony Busker
             </div>
         </footer>
 
