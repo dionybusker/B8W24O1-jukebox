da112d4f7ba06abcc0c9e2e43d52fc908707a0ce
Diony <dionybusker@gmail.com>
2021-12-11 16:58:53 +0100

    Deleted table for now

--- resources/views/playlists.blade.php
+++ resources/views/playlists.blade.php
@@ -9,36 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-    {{--            {{ Session::get('song') }}--}}
-
-    {{--                @if (Session::has('song'))--}}
-    {{--                    @foreach (Session::get('song') as $song)--}}
-    {{--                        {{ $song }}--}}
-    {{--                    @endforeach--}}
-    {{--                @endif--}}
-
-    {{--            {{ dd(Session::get('song')) }}--}}
-                    @if (Session::has('song'))
-                        <div class="flex justify-center mx-auto">
-                            <div class="flex flex-col">
-                                <div class="w-full">
-                                    <div class="border-b border-gray-200 shadow">
-                                        <x-table.table :headers="['Name', 'Genre', 'Artist', 'Duration']">
-        {{--                                    @foreach (Session::get('song') as $song)--}}
-                                                <tr class="whitespace-nowrap">
-
-                                                    <x-table.td>{{ Session::get('song')['name'] }}</x-table.td>
-                                                    <x-table.td>{{ Session::get('song')['genre'] }}</x-table.td>
-                                                    <x-table.td>{{ Session::get('song')['artist'] }}</x-table.td>
-                                                    <x-table.td>{{ Session::get('song')['duration'] }}</x-table.td>
-                                                </tr>
-        {{--                                    @endforeach--}}
-                                        </x-table.table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endif
+                    Hier worden de OPGESLAGEN playlists weergegeven
                 </div>
             </div>
         </div>
