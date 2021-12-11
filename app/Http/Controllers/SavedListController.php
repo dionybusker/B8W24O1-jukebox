d804a475448699a890821a7ce95101f82a525b71
Diony <dionybusker@gmail.com>
2021-12-11 17:00:06 +0100

    Playlist class w.i.p.

--- app/Http/Controllers/SavedListController.php
+++ app/Http/Controllers/SavedListController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes\Playlist;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use App\Http\Requests;
 use App\Models\Song;
 use App\Http\Controllers\Controller;
+//use App\Classes\Playlist;
 
 class SavedListController extends Controller
 {
@@ -31,49 +33,51 @@ class SavedListController extends Controller
 
         public function session(Request $request)
         {
+             new Playlist('test');
+
 //            Session::put('song', $request->all());
 //            $data = Song::get()->where('id', $request->id);
-            $data = Song::find($request->id);
-
-//            dd($data->name);
-
-//            $sessionSongs = Session::get('song');
-
-//            dd($sessionSongs);
-
-            $song = [
-                'songId' => $data->id,
-                'name' => $data->name,
-                'genre' => $data->genre->name,
-                'artist' => $data->artist,
-                'duration' => $data->duration
-            ];
-
-            if ($request->session()->exists('song')) {
-                // Wat er nu gebeurt:
-                // De huidige session wordt opgeslagen in $value
-                // $value wordt opgehaald en in een nieuwe array $new geplaatst
-                // Naast $value wordt ook $song in $new geplaatst
-                // $value wordt alleen steeds opnieuw opgeslagen
-                // Dus ik moet denk ik een array leegmaken voordat het opnieuw opgeslagen wordt
-
-
-//                $request->session()->push('song', $song);
-//                $request->session()->forget('song');
-
-//                $value = $request->session()->pull('song', 'default'); // array
+//            $data = Song::find($request->id);
 //
-//                $new = [ $value, $song ];
-//                dd($new);
-                $request->session()->put('song', $song);
-
-//                dd($value);
-
-//                $newSession = [$value, $song];
-
-            } else {
-                $request->session()->put('song', $song);
-            }
+////            dd($data->name);
+//
+////            $sessionSongs = Session::get('song');
+//
+////            dd($sessionSongs);
+//
+//            $song = [
+//                'songId' => $data->id,
+//                'name' => $data->name,
+//                'genre' => $data->genre->name,
+//                'artist' => $data->artist,
+//                'duration' => $data->duration
+//            ];
+//
+//            if ($request->session()->exists('song')) {
+//                // Wat er nu gebeurt:
+//                // De huidige session wordt opgeslagen in $value
+//                // $value wordt opgehaald en in een nieuwe array $new geplaatst
+//                // Naast $value wordt ook $song in $new geplaatst
+//                // $value wordt alleen steeds opnieuw opgeslagen
+//                // Dus ik moet denk ik een array leegmaken voordat het opnieuw opgeslagen wordt
+//
+//
+////                $request->session()->push('song', $song);
+////                $request->session()->forget('song');
+//
+////                $value = $request->session()->pull('song', 'default'); // array
+////
+////                $new = [ $value, $song ];
+////                dd($new);
+//                $request->session()->put('song', $song);
+//
+////                dd($value);
+//
+////                $newSession = [$value, $song];
+//
+//            } else {
+//                $request->session()->put('song', $song);
+//            }
 
 //            dd($request);
 
