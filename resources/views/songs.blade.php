21a36b441f339c89dd63d6aee7fdc657d8c46fad
Diony Busker <dionybusker@gmail.com>
2021-12-16 21:01:24 +0100

    Added a "Sort by genre" button (no functionality yet)

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -2,15 +2,25 @@
     <x-slot name="header">
 {{--        @include('_header')--}}
 
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Songs') }}
-        </h2>
+        <div class="flex justify-between">
+            <h2 class="inline-block font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Songs') }}
+            </h2>
+
+            <div>
+                <button class="block lg:inline-block lg:text-center lg:text-white lg:mt-0 lg:ml-3 lg:px-3 lg:py-1 pl-2 py-3 rounded lg:uppercase lg:text-xs lg:font-semibold lg:bg-blue-600 lg:hover:bg-blue-500 lg:hover:text-white hover:bg-gray-200 hover:text-gray-800">
+                    <a href="">
+                        {{ __('Sort by genre') }}
+                    </a>
+                </button>
+            </div>
+        </div>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+{{--        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">--}}
+{{--            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">--}}
+{{--                <div class="p-6 bg-white border-b border-gray-200">--}}
                     <div class="lg:w-2/3 w-full mx-auto flex items-center lg:grid lg:grid-cols-4">
                         @foreach ($songs as $song)
                             <div class="p-2">
@@ -22,9 +32,9 @@
                                     </div>
 
                                     <div class="px-4 py-2 overflow-auto h-40">
-                                        <p class="font-semibold">{{ $song->name }}</p>
-                                        <p>{{ $song->artist }}</p>
-                                        <p>{{ $song->length }} minutes</p>
+                                        <p><span class="font-semibold">Title:</span> {{ $song->name }}</p>
+                                        <p><span class="font-semibold">Artist:</span> {{ $song->artist }}</p>
+                                        <p><span class="font-semibold">Length:</span> {{ $song->length }}</p>
                                     </div>
 
                                     <form action="{{ route('songs') }}/{{ $song->id }}" method="post">
@@ -58,8 +68,8 @@
                             </div>
                         @endforeach
                     </div>
-                </div>
-            </div>
-        </div>
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
     </div>
 </x-app-layout>
