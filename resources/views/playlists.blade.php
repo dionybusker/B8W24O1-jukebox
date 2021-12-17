a2b81df3d4400039f430ecda40fa1894338d2dc4
Diony <dionybusker@gmail.com>
2021-12-17 14:27:27 +0100

    Created playlists are shown on page

--- resources/views/playlists.blade.php
+++ resources/views/playlists.blade.php
@@ -8,8 +8,37 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    Hier worden de OPGESLAGEN playlists weergegeven
+                <div class="bg-white border-b border-gray-200">
+{{--                    {{ dd($playlists) }}--}}
+                    <x-table.table :headers="['Name', 'Total songs', 'Total length', {{-- 'Details', --}} 'Delete playlist']">
+                        @foreach ($playlists as $playlist)
+                            <tr class="whitespace-nowrap">
+                                <x-table.td>
+                                    <a href="#">
+                                        {{ $playlist->name }}
+                                    </a>
+                                </x-table.td>
+                                <x-table.td><!-- total songs --></x-table.td>
+                                <x-table.td><!-- total length --></x-table.td>
+{{--                                <x-table.td>--}}
+{{--                                    <form action="" method="post">--}}
+{{--                                        @csrf--}}
+{{--                                        <button class="p-2 w-1/2 bg-red-600 hover:bg-red-500 rounded rounded-md text-white text-center py-1">--}}
+{{--                                            <i class="fas fa-info"></i>--}}
+{{--                                        </button>--}}
+{{--                                    </form>--}}
+{{--                                </x-table.td>--}}
+                                <x-table.td>
+                                    <form action="" method="post">
+                                        @csrf
+                                        <button class="w-1/2 bg-red-600 hover:bg-red-500 rounded rounded-md text-white text-center py-1">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </button>
+                                    </form>
+                                </x-table.td>
+                            </tr>
+                        @endforeach
+                    </x-table.table>
                 </div>
             </div>
         </div>
