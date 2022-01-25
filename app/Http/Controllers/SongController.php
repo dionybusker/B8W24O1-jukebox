17889ed5f08f2c13709c4a03413a7e59c5877fb0
Diony <dionybusker@gmail.com>
2022-01-25 11:18:56 +0100

    Cleaning up

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -2,17 +2,11 @@
 
 namespace App\Http\Controllers;
 use App\Models\Genre;
-use App\Models\PlaylistSong;
 use App\Models\Song;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class SongController extends Controller
 {
     public function index() {
-//        $songs = Song::with('genre')->paginate(12);
-
         return view('songs', [
             'genres' => Genre::get(),
             'songs' => Song::latest()->filter(
@@ -20,28 +14,4 @@ class SongController extends Controller
             )->paginate(12)->withQueryString()
         ]);
     }
-
-//    public function create()
-//    {
-//        return view('songs');
-//    }
-//
-//    public function store(Request $request)
-//    {
-//        $request->validate([
-//            'genre_id' => ['required', 'int'],
-//            'name' => ['required', 'string', 'max:255'],
-//            'artist' => ['required', 'string', 'max:255'],
-//            'length' => ['required', 'int'],
-//        ]);
-//
-//        Song::create([
-//            'genre_id' => $request->genre_id,
-//            'name' => $request->name,
-//            'artist' => $request->artist,
-//            'length' => $request->duration,
-//        ]);
-//
-//        return redirect(RouteServiceProvider::HOME);
-//    }
 }
